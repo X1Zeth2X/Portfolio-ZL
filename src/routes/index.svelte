5ef6cce1e5d2c7530d072ec6d929bdf4141152e8
Zeth <zeth.leonardo@protonmail.com>
2020-02-10 20:37:09 +0400

    Removed content clipping.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,13 +2,6 @@
   import Intro from "../components/Intro.svelte";
 </script>
 
-<style>
-.content {
-  clip-path: polygon(0 35%, 100% 0, 100% 100%, 0% 100%);
-  background-color: #31384A;
-}
-</style>
-
 <svelte:head>
 	<title>
     Hom sweet home
@@ -18,5 +11,6 @@
 <!-- Landing view -->
 <Intro />
 
-<div class="content bg-blue-600 py-20">
+<div class="py-12 h-100 text-5xl text-center">
+Noice
 </div>
